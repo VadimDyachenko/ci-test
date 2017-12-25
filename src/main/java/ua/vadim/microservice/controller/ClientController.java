3bc2c5643eb694dfa21af8c98f9b4a6e9a115e4a
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2017-12-25 14:28:00 +0200

    First application configuration

---
+++ src/main/java/ua/vadim/microservice/controller/ClientController.java
@@ -0,0 +1,30 @@
+package ua.vadim.microservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.http.ResponseEntity.badRequest;
+import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+@RestController
+@RequestMapping(path = "clients")
+public class ClientController {
+    private static final List<String> CLIENTS = new ArrayList<>();
+
+    @RequestMapping(path = "", method = POST)
+    public ResponseEntity<?> writeDownClient(@RequestBody Map<String, Object> request) {
+        if (request.containsKey("name") && request.get("name") != null) {
+            CLIENTS.add(request.get("name").toString());
+            return ok(CLIENTS);
+        } else {
+            return badRequest().body("'name' is required.");
+        }
+    }
+}
---
+++ src/test/java/ua/vadim/microservice/ClientControllerTest.java
@@ -0,0 +1,50 @@
+package ua.vadim.microservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import javax.inject.Inject;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import ua.vadim.microservice.controller.ClientController;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ClientController.class)
+public class ClientControllerTest {
+
+    @Inject
+    private MockMvc mockMvc;
+
+    @Test
+    public void create_ok() throws Exception {
+        mockMvc
+                .perform(post("/clients").contentType(APPLICATION_JSON).content("{\"name\": \"Super Man\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(hasSize(1)))
+                .andExpect(jsonPath("$[0]").value("Super Man"));
+
+        mockMvc
+                .perform(post("/clients").contentType(APPLICATION_JSON).content("{\"name\": \"Iron man\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(hasSize(2)))
+                .andExpect(jsonPath("$[0]").value("Super Man"))
+                .andExpect(jsonPath("$[1]").value("Iron man"));
+    }
+
+    @Test
+    public void create_badRequest() throws Exception {
+        mockMvc
+                .perform(post("/clients").contentType(APPLICATION_JSON).content("{\"typoName\": \"Hudson Mendes\"}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("'name' is required."));
+    }
+}
