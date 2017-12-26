287e562c6e2180d754bef893035b0ecfae652236
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2017-12-26 11:08:01 +0200

    test changed config

--- _global/package-service.sh
+++ _global/package-service.sh
@@ -3,16 +3,12 @@ tag_and_push_image() {
   ACCOUNT_NAME='vadimdyachenko'
 
   echo "building image $1"
-  sudo docker build -t $ACCOUNT_NAME/$1:$BRANCH.$SHIPPABLE_BUILD_NUMBER .
+
+  COMMIT_HASH=expr substr $COMMIT 1 10
+  DATE=`date +%Y-%m-%d`
+  sudo docker build -t $ACCOUNT_NAME/$1:$PULL_REQUEST_BASE_BRANCH_$DATE-$COMMIT_HASH .
   echo "pushing image $1"
-  sudo docker push $ACCOUNT_NAME/$1:$BRANCH.$SHIPPABLE_BUILD_NUMBER
-
-  # We trigger the manifest and subsequently the deploy jobs downstream by posting a new version to the image resource.
-  # Since the image resource is an INPUT to the manifest job, the manifest job will get scheduled to run after these steps.
-#  echo "posting the version of the image resource for $1"
-#  shipctl put_resource_state $1"_img" "SHIPPABLE_BUILD_NUMBER" $SHIPPABLE_BUILD_NUMBER
-#  shipctl put_resource_state $1"_img" "versionName" $BRANCH.$SHIPPABLE_BUILD_NUMBER
-
+  sudo docker push $ACCOUNT_NAME/$1:$PULL_REQUEST_BASE_BRANCH_$DATE-$COMMIT_HASH
 }
 
 main() {
