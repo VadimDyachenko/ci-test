133eced69c34df487ce4d0b6a3a56512da7cd123
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2017-12-26 19:05:28 +0200

    test changed config

--- _global/package-service.sh
+++ _global/package-service.sh
@@ -3,15 +3,13 @@ tag_and_push_image() {
   ACCOUNT_NAME='vadimdyachenko'
 
   echo "building image $1"
-  echo "building commit $2"
-  echo "building pullrequest $3"
+  echo "building commit $COMMIT"
 
-#  COMMIT_HASH="$(expr substr $2 1 10)"
-  COMMIT_HASH="12345"
+  COMMIT_HASH="$(expr substr $COMMIT 1 10)"
   DATE="$(date +%Y-%m-%d)"
-  sudo docker build -t $ACCOUNT_NAME/$1:master.$DATE-$COMMIT_HASH .
+  sudo docker build -t $ACCOUNT_NAME/$1:$BRANCH.$DATE-$COMMIT_HASH .
   echo "pushing image $1"
-  sudo docker push $ACCOUNT_NAME/$1:master.$DATE-$COMMIT_HASH
+  sudo docker push $ACCOUNT_NAME/$1:$BRANCH.$DATE-$COMMIT_HASH
 }
 
 main() {
