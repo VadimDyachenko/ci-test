e5c711026f1a2a26f2f56565dc66253246a16449
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2017-12-28 07:53:49 +0200

    test changed config

--- detect-changed-services.sh
+++ detect-changed-services.sh
@@ -5,15 +5,15 @@ detect_changed_services() {
  echo "detecting changed folders for this commit"
 
  # get a list of all the changed folders only
- changed_folders=`git diff --name-only $SHIPPABLE_COMMIT_RANGE | grep / | awk 'BEGIN {FS="/"} {print $1}' | uniq`
+ changed_folders="$(git diff --name-only $SHIPPABLE_COMMIT_RANGE | grep / | awk 'BEGIN {FS="/"} {print $1}' | uniq)"
  echo "changed folders: "$changed_folders
 
  changed_services=()
- for folder in $changed_folders
+ for folder in "${changed_folders[@]}"
  do
    if [ "$folder" == '_global' ]; then
      echo "common folder changed, building and publishing all microservices"
-     changed_services=`find . -maxdepth 1 -type d -not -name '_global' -not -name 'shippable' -not -name '.git' -not -path '.' | sed 's|./||'`
+     changed_services="$(find . -maxdepth 1 -type d -not -name '_global' -not -name 'shippable' -not -name '.git' -not -path '.' | sed 's|./||')"
      echo "list of microservice "$changed_services
      break
    else
@@ -29,10 +29,10 @@ detect_changed_services() {
  do
    echo "-------------------Running packaging for $service---------------------"
    # copy the common code to the service so that it can be packaged in the docker image
-   cp -r ./_global $service
+   cp package-service.sh $service
    pushd "$service"
    # move the build script to the root of the service
-   mv ./_global/package-service.sh ./.
+   # mv ./_global/package-service.sh ./.
    ./package-service.sh "$service"
    popd
  done
--- orders/src/main/java/service/orders/controller/OrderController.java
+++ orders/src/main/java/service/orders/controller/OrderController.java
@@ -28,7 +28,4 @@ public class OrderController {
         }
     }
 //Added changes
-//Added changes
-//Added changes
-//Added changes
 }
