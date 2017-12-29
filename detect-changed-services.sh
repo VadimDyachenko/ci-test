7810508ca8283b8a50488dd3f5b3047e080a857b
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2017-12-29 09:50:25 +0200

    changed config  detect_changed_services

--- detect-changed-services.sh
+++ detect-changed-services.sh
@@ -5,24 +5,16 @@ detect_changed_services() {
  echo "detecting changed folders for this commit"
 
  changed_folders="$(git diff --name-only $SHIPPABLE_COMMIT_RANGE | grep / | awk 'BEGIN {FS="/"} {print $1}' | uniq)"
- echo `git rev-parse MERGE_HEAD`
  echo "changed folders: "$changed_folders
 
- changed_services=()
  for folder in $changed_folders
  do
-     echo "Adding $folder to list of services to build"
-     changed_services+=("$folder")
- done
-
- for service in "${changed_folders[@]}"
- do
-   echo "-------------------Running packaging for $service---------------------"
-   cp package-service.sh $service
-   pushd "$service"
+   echo "-------------------Running packaging for $folder---------------------"
+   cp package-service.sh $folder
+   pushd "$folder"
    if [ -f "Dockerfile" ];
-    then ./package-service.sh "$service"
-    else echo "Dockerfile not present in $service. Packaging skipped"
+    then ./package-service.sh "$folder"
+    else echo "Dockerfile not present in $folder. Packaging skipped"
    fi
    popd
  done
