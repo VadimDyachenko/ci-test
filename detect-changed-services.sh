0824770dfba7ef238816b3a1294dc6ecd561ab26
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2018-03-25 15:55:38 +0300

    changed shippable config

--- detect-changed-services.sh
+++ detect-changed-services.sh
@@ -4,10 +4,7 @@ detect_changed_services() {
  echo "----------------------------------------------"
  echo "detecting changed folders for this commit"
 
- changed_folders="$(git diff --name-only $SHIPPABLE_COMMIT_RANGE | grep / | awk 'BEGIN {FS="/"} {print $1}' | uniq)"
- echo "changed folders: "$changed_folders
-
- for folder in $changed_folders
+ for folder in  */
  do
    echo "-------------------Running packaging for $folder---------------------"
    cp package-service.sh $folder
