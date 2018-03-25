090df927403d25af7111cb137be8136f9d02db9c
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2018-03-25 17:14:11 +0300

    changed shippable config

--- detect-changed-services.sh
+++ detect-changed-services.sh
@@ -4,13 +4,13 @@ detect_changed_services() {
  echo "----------------------------------------------"
  echo "detecting changed folders for this commit"
 
- for folder in *
+ for folder in */
  do
    echo "-------------------Running packaging for $folder---------------------"
    cp package-service.sh $folder
    pushd "$folder"
    if [ -f "Dockerfile" ];
-    then ./package-service.sh "$folder"
+    then ./package-service.sh "$folder%%/"
     else echo "Dockerfile not present in $folder. Packaging skipped"
    fi
    popd
