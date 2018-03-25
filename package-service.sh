9204f40bac2907019f32231b65605ec380ebb87f
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2018-03-25 16:07:57 +0300

    changed shippable config

--- package-service.sh
+++ package-service.sh
@@ -13,7 +13,6 @@ main() {
 
   COMMIT_HASH="$(expr substr $COMMIT 1 10)"
   DATE="$(date +%Y-%m-%d)"
-  sudo docker run --name=gradle -u root --rm --volumes-from $SHIPPABLE_CONTAINER_NAME -w /build romach007/gradle-build:1.0 gradle clean assemble --stacktrace
   sudo docker build -t $ACCOUNT_NAME/${SERVICES[$1]}:$BRANCH.$DATE-$COMMIT_HASH .
   echo "pushing image $1"
   sudo docker push $ACCOUNT_NAME/${SERVICES[$1]}:$BRANCH.$DATE-$COMMIT_HASH
